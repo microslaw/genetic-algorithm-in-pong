0e306390a06a61279a7838e8c9f12ef0500c2ea8
microslaw <milosz.grunwald@gmail.com>
2022-08-19 15:13:20 +0200

    added saving games

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 import random
-import this
 import pygame
+import os
 
 bouncerHeight = 32
 bouncerSpeed = 0
@@ -9,6 +9,7 @@ IsBallIn = True
 p1Bouncer = None
 p2Bouncer = None
 winner = None
+attemptNo = 1
 
 def sgn(x):
     if x>0:return 1
@@ -21,11 +22,14 @@ class Ball:
         self.y = y
         self.xSpeed = xSpeed
         self.ySpeed = ySpeed
-        self.modifier
+        #timeModifier speeds up the ball over time (time is measured in bounces)
+        self.timeModifier = 1
+        #bounceModifier speeds up the ball based on where it hits bouncer
+        self.bounceModifier = 1
 
     def move(self):
-        dx = self.xSpeed * self.modifier
-        dy = self.ySpeed * self.modifier
+        dx = self.xSpeed * self.bounceModifier * self.timeModifier
+        dy = self.ySpeed * self.bounceModifier * self.timeModifier
         self.x -= dx
         self.y -= dy
 
@@ -75,11 +79,11 @@ class Ball:
             return 0
 
         if self.x == 1:
-            modifier = p1Bouncer.get_bounce_modifier(p1Bouncer, self.y)
+            bounceModifier = p1Bouncer.get_bounce_modifier(p1Bouncer, self.y)
             bounceCount += 1
                 
         if self.x == 254:
-            modifier = p1Bouncer.get_bounce_modifier(p1Bouncer, self.y)
+            bounceModifier = p1Bouncer.get_bounce_modifier(p1Bouncer, self.y)
             bounceCount += 1
 
 class Bouncer:
@@ -96,7 +100,7 @@ class Bouncer:
         self.lowEnd -= bouncerSpeed
         self.highEnd -= bouncerSpeed    
 
-# bounce modifier changes the speed of ball, if bouncer misses the ball returns 0 
+# bounce bounceModifier changes the speed of ball, if bouncer misses the ball returns 0 
     def get_bounce_modifier(self, y):
         if self.lowEnd>=y and self.highEnd<= y:
             distanceToY = abs(y-self.highEnd - bouncerHeight//2)
@@ -108,48 +112,64 @@ class Bouncer:
         IsBallIn = False
 
 
-def play_a_game(players, seed = random.randrange(1016)):
+def play_a_game(players: tuple, seed = random.randrange(1016)):
     p1,p2 = players
     isBallIn = True
     bounceCount = 0
 
     p1Bouncer = Bouncer(1)
     p2Bouncer = Bouncer(254)
-    ball = Ball()
+    ball = Ball(seed%254+1,(seed%2)*2-1, ((seed%2)%2)*2-1)
 
     while isBallIn:
-
+        ball.timeModifier = round(bounceCount*bounceCount*0.01 + 1)
         ball.move()
         
         p1Action = p1.decide()
         p2Action = p2.decide()
 
-        
-        if p1Action == "up":
+        if p1Action == -1:
             p1Bouncer.up()
-        elif p1Action == "down":
+        elif p1Action == 1:
             p1Bouncer.down()
 
-        if p2Action == "up":
+        if p2Action == -1:
             p2Bouncer.up()
-        elif p2Action == "down":
+        elif p2Action == 1:
             p2Bouncer.down()
 
+    #returns data needed to save, cannot save here because does not know gen and gameId
+    #log[0] is idex of winner in players
     if ball.x == 0:
-        return 0
+        return (0, bounceCount, seed)
     else:
-        return 1
+        return (1, bounceCount, seed)
 
+def fill0(num, lenght = 4):
+    num = str(num)
+    return ("0"*(lenght-len(num))) + num
 
+def save_game(players, log, gen, gameId):
+    file = f"\\genetic-algorithm-in-pong\\data\\attempt{attemptNo}\\gen{fill0(gen)}\\games\\game{fill0(gameId)}gen{fill0(gen)}.txt"
+    filepath = os.getcwd() + file
+    toWrite = f"{players[0].name} vs {players[1].name}/n"
+    towrite +=f"{log[2]}/n"
+    towrite +=f"{log[0]}/n"
+    towrite +=f"{log[1]}/n"
 
-def read_game(filename):
-    with open(filename, 'r') as rfile:
-        return rfile.read()
+    with open(filepath, 'w') as wfile:
+        wfile.write(towrite)
 
-def save_game(filename):
-    with open(filename, 'w') as wfile:
-        return wfile.write()
+def read_game(gen, gameId):
+    file = f"\\genetic-algorithm-in-pong\\data\\attempt{attemptNo}\\gen{fill0(gen)}\\games\\game{fill0(gameId)}gen{fill0(gen)}.txt"
+    filepath = os.getcwd() + file
+    with open(filepath, 'r') as rfile:
+        toRead = rfile.read()
 
 def draw_game(ball, p1,p2):
     pass
 
+print(os.path.realpath(__file__))
+
+# with open(os.getcwd() + filepath, 'w') as wfile:
+#     wfile.write("you found me")
