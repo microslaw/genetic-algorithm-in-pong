16df7e29a9ea6e5e507c324972cda230e3a19ea8
microslaw <milosz.grunwald@gmail.com>
2022-08-26 10:54:42 +0200

    backup before rewriting broken down vector colision

--- game.py
+++ game.py
@@ -1,13 +1,15 @@
 import random
 import pygame
+from neuralNetwork import * 
 import os
+
+
 
 bouncerHeight = 32
 bouncerSpeed = 0
 bounceCount = 0
 IsBallIn = True
-p1Bouncer = None
-p2Bouncer = None
+
 winner = None
 attemptNo = 1
 
@@ -24,118 +26,169 @@ class Ball:
         self.ySpeed = ySpeed
         #timeModifier speeds up the ball over time (time is measured in bounces)
         self.timeModifier = 1
-        #bounceModifier speeds up the ball based on where it hits bouncer
-        self.bounceModifier = 1
+        #bounceSpotModifier speeds up the ball based on where it hits bouncer
+        self.bounceSpotModifier = 1
 
     def move(self):
-        dx = self.xSpeed * self.bounceModifier * self.timeModifier
-        dy = self.ySpeed * self.bounceModifier * self.timeModifier
-        self.x -= dx
-        self.y -= dy
+        print(self.xSpeed, self.bounceSpotModifier, self.timeModifier)
+        dx = self.xSpeed * self.bounceSpotModifier * self.timeModifier
+        dy = self.ySpeed * self.bounceSpotModifier*2
+        self.x += dx
+        self.y += dy
 
-        if self.x>255 or self.x<0 or self.y >255 or self.y<0:
-            self.x += dx
-            self.y += dy
+        print("maybe out of bounds", self.x, self.y)
+        #if ball is out of bounds, backtrack to the edge of the screen where it should bounce
+        if self.x>253 or self.x<2 or self.y >254 or self.y<1:
+            self.x -= dx
+            self.y -= dy
+            sdx = sgn(dx)
+            sdy = sgn(dy)
+
 
             #breaking velocity vector into smaller to implement collision
             while (abs(dx) + abs(dy)>0):
+                print("maybe out of bounds backtracking", self.x, self.y)
                 if abs(dx)>abs(dy):
                     v = (round(dx/abs(dy)),sgn(dy))
                 elif abs(dx)<abs(dy):
-                    v = (round(dy/abs(dx)),sgn(dx))
+                    v = (sgn(dx),round(dy/abs(dx)))
                 else:
-                    v = (1,1)
-                
+                    v = (sgn(dx),sgn(dy))
+
                 dx -= v[0]
                 dy -= v[1]
                 
-                if abs(dx) > abs(dy):
-                    for i in range(v[0]+1):
+
+                if abs(v[0]) == 1 and abs(v[1]) == 1:
+                    self.y +=sdy
+                    print("pre check", self.x, self.y, sdx, sdy)
+                    if self.is_bouncing():
+                        break
+                    self.x +=sdx
+                    if self.is_bouncing():
+                        break
+                if abs(v[1]) == 1:
+                    for i in range(abs(v[0])+1):
                         if i == v[0]//2:
-                            self.y +=1
+                            self.y +=sdy
                         else:
-                            self.x +=1
-                        self.try_bouncing()
-                else:
-                    for i in range(v[1]+1):
+                            self.x +=sdx
+                        if self.is_bouncing():
+                            break
+                if abs(v[0]) == 1:
+                    for i in range(abs(v[1])+1):
                         if i == v[1]//2:
-                            self.x +=1
-                            continue
-                        self.y +=1
+                            self.x +=sdx
+                        else:
+                            self.y +=sdy
+                        if self.is_bouncing():
+                            break
 
-    def try_bouncing(self):
+                #self.x += dx
+                #self.y += dy
 
-        if self.y == 0:
+
+    def is_bouncing(self):
+
+        global bounceCount
+
+        print("directions", self.xSpeed, self.ySpeed)
+
+        if self.y == 0 and  self.ySpeed < 0:
             tmp = self.ySpeed
             self.ySpeed = abs(self.xSpeed)
             self.xSpeed = abs(tmp) * sgn(self.xSpeed)
-            bounceCount+=1
-            return 0
-        if self.y == 255:
+            return True
+
+        if self.y == 255 and self.ySpeed > 0:
             tmp = self.ySpeed
             self.ySpeed = abs(self.xSpeed) * -1
             self.xSpeed = abs(tmp) * sgn(self.xSpeed)
-            bounceCount+=1
-            return 0
+            return True
+        print("self.x: ",self.x)
 
-        if self.x == 1:
-            bounceModifier = p1Bouncer.get_bounce_modifier(p1Bouncer, self.y)
+        if self.x == 1 and self.xSpeed < 0:
+            print("bounce modifier")
+            self.bounceSpotModifier = p1Bouncer.get_bounce_modifier(self.y)
             bounceCount += 1
-                
-        if self.x == 254:
-            bounceModifier = p1Bouncer.get_bounce_modifier(p1Bouncer, self.y)
+            return True
+
+        if self.x == 254 and self.xSpeed > 0:
+            self.bounceSpotModifier = p2Bouncer.get_bounce_modifier(self.y)
             bounceCount += 1
+            return True
+
+        
+        return False
 
 class Bouncer:
     def __init__(self, posX) -> None:
         self.posX = posX
-        self.lowEnd = 128+bouncerHeight//2
-        self.highEnd = 128-bouncerHeight//2
+        self.lowEnd = 128 + bouncerHeight//2
+        self.highEnd = 128 - bouncerHeight//2
+        self.middle = 128
 
     def up(self):
         self.lowEnd -= bouncerSpeed
+        self.middle -= bouncerSpeed
         self.highEnd -= bouncerSpeed    
 
     def down(self):
-        self.lowEnd -= bouncerSpeed
-        self.highEnd -= bouncerSpeed    
+        self.lowEnd += bouncerSpeed
+        self.middle += bouncerSpeed
+        self.highEnd += bouncerSpeed    
 
-# bounce bounceModifier changes the speed of ball, if bouncer misses the ball returns 0 
+# bounce bounceSpotModifier changes the speed of ball, if bouncer misses the ball returns 0 
     def get_bounce_modifier(self, y):
-        if self.lowEnd>=y and self.highEnd<= y:
+        
+        if self.lowEnd >= y and self.highEnd <= y:
             distanceToY = abs(y-self.highEnd - bouncerHeight//2)
-            if distanceToY<bouncerHeight//8:
+            if distanceToY < bouncerHeight//8:
                 return 2
-            if distanceToY<bouncerHeight//4:
+            if distanceToY < bouncerHeight//4:
                 return 1.5
             return 1
+        global IsBallIn
         IsBallIn = False
+        exit()
+        return 0
 
 
 def play_a_game(players: tuple, seed = random.randrange(1016)):
+    
+    p1 : Player
+    p2 : Player
     p1,p2 = players
+    global IsBallIn
     isBallIn = True
+    global bounceCount
     bounceCount = 0
+    global p1Bouncer, p2Bouncer
 
-    p1Bouncer = Bouncer(1)
-    p2Bouncer = Bouncer(254)
+    p1Bouncer = Bouncer(0)
+    p2Bouncer = Bouncer(255)
     ball = Ball(seed%254+1,(seed%2)*2-1, ((seed%2)%2)*2-1)
+    timer = 0
+
+    initialize_drawing()
 
     while isBallIn:
+        draw_game(ball, p1Bouncer, p2Bouncer)
+
+        #print(timer)
+        timer += 1
         ball.timeModifier = round(bounceCount*bounceCount*0.01 + 1)
         ball.move()
-        
-        p1Action = p1.decide()
-        p2Action = p2.decide()
-
-        if p1Action == -1:
+        p1Action = p1.decide(p2Bouncer.middle, p1Bouncer.middle, ball.y, ball.x, bounceCount)
+        p2Action = p2.decide(p1Bouncer.middle, p2Bouncer.middle, ball.y, 256 - ball.x, bounceCount)
+        if p1Action == 0:
             p1Bouncer.up()
-        elif p1Action == 1:
+        elif p1Action == 2:
             p1Bouncer.down()
 
-        if p2Action == -1:
+        if p2Action == 0:
             p2Bouncer.up()
-        elif p2Action == 1:
+        elif p2Action == 2:
             p2Bouncer.down()
 
     #returns data needed to save, cannot save here because does not know gen and gameId
@@ -145,20 +198,16 @@ def play_a_game(players: tuple, seed = random.randrange(1016)):
     else:
         return (1, bounceCount, seed)
 
-def fill0(num, lenght = 4):
-    num = str(num)
-    return ("0"*(lenght-len(num))) + num
-
 def save_game(players, log, gen, gameId):
     file = f"\\genetic-algorithm-in-pong\\data\\attempt{attemptNo}\\gen{fill0(gen)}\\games\\game{fill0(gameId)}gen{fill0(gen)}.txt"
     filepath = os.getcwd() + file
     toWrite = f"{players[0].name} vs {players[1].name}/n"
-    towrite +=f"{log[2]}/n"
-    towrite +=f"{log[0]}/n"
-    towrite +=f"{log[1]}/n"
+    toWrite +=f"{log[2]}/n"
+    toWrite +=f"{log[0]}/n"
+    toWrite +=f"{log[1]}/n"
 
     with open(filepath, 'w') as wfile:
-        wfile.write(towrite)
+        wfile.write(toWrite)
 
 def read_game(gen, gameId):
     file = f"\\genetic-algorithm-in-pong\\data\\attempt{attemptNo}\\gen{fill0(gen)}\\games\\game{fill0(gameId)}gen{fill0(gen)}.txt"
@@ -166,10 +215,24 @@ def read_game(gen, gameId):
     with open(filepath, 'r') as rfile:
         toRead = rfile.read()
 
-def draw_game(ball, p1,p2):
-    pass
+def initialize_drawing():
+    pygame.init()
+    global win 
+    win = pygame.display.set_mode((256,256))
+    pygame.display.set_caption("Pong")
 
-print(os.path.realpath(__file__))
+def draw_game(ball, p1Bouncer, p2Bouncer):
 
-# with open(os.getcwd() + filepath, 'w') as wfile:
-#     wfile.write("you found me")
+    global IsBallIn
+    global win
+    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            IsBallIn = False
+    pygame.draw.rect(win, (255,0,0), pygame.Rect(ball.x, ball.y, 1, 1))
+    pygame.draw.rect(win, (0,0,255), pygame.Rect(p1Bouncer.posX, p1Bouncer.lowEnd, 1, p1Bouncer.lowEnd - p1Bouncer.highEnd))
+    pygame.draw.rect(win, (0,255,0), pygame.Rect(p2Bouncer.posX, p2Bouncer.lowEnd, 1, p2Bouncer.lowEnd - p2Bouncer.highEnd))
+
+print(play_a_game((Player(0,0,16, "2065216056286540731964788133229977355704872542009971429237186953114294405058590826391403880056898570023122232212663581462261372286922779"),
+             Player(0,0,16, "2962002178200263828017493067689927259614674561082926627623621204269956299544861619071519854706114178017987120872598502206647480500149283")),
+             1000))
